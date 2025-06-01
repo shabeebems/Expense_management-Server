dbfbfd003f36a6ecfb14a8bc233d8a60fade49df
Muhammed Shabeeb <muhammedshabeeb330@gmail.com>
2025-06-01 15:10:28 +0530

    second commit

--- src/config/cors.js
+++ src/config/cors.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
-
+const VERCEL_FRONTEND_URL = process.env.VERCEL_FRONTEND_URL
 export const corsMiddleware = cors({
-    origin: [FRONTEND_URL],
+    origin: [FRONTEND_URL, VERCEL_FRONTEND_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     // origin: "http://localhost:5173",

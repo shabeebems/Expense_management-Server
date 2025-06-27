6bd14e06ebdfb9fef69246ea35831851a73689b0
Muhammed Shabeeb <muhammedshabeeb330@gmail.com>
2025-06-27 22:54:34 +0530

    token validation

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,10 +5,10 @@ const userRouter = express.Router()
 
 // User
 userRouter.post('/create_order', authenticateToken, userController.createOrder);
-userRouter.get('/fetch_orders/:statusFilter', userController.getOrders);
-userRouter.get('/fetch_single_order/:orderId', userController.getSingleOrder);
-userRouter.post('/add_expense/:orderId', userController.addExpense);
-userRouter.post('/add_income/:orderId', userController.addIncome);
-userRouter.get('/fetch_income_and_expense/:orderId', userController.fetchIncomeAndExpense);
+userRouter.get('/fetch_orders/:statusFilter', authenticateToken, userController.getOrders);
+userRouter.get('/fetch_single_order/:orderId', authenticateToken, userController.getSingleOrder);
+userRouter.post('/add_expense/:orderId', authenticateToken, userController.addExpense);
+userRouter.post('/add_income/:orderId', authenticateToken, userController.addIncome);
+userRouter.get('/fetch_income_and_expense/:orderId', authenticateToken, userController.fetchIncomeAndExpense);
 
 export default userRouter

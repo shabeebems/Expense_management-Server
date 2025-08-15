0db3891ec81e4b9feb841ebd154225d81f241c6f
Muhammed Shabeeb <muhammedshabeeb330@gmail.com>
2025-08-15 23:41:25 +0530

    Refactored user role system: merged two user roles into single role structure and updated logic successfully

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,6 @@ import userController from '../controller/user.controller.js'
 import { authenticateToken } from '../middleware/tokenValidation.js';
 const userRouter = express.Router()
 
-userRouter.get('/run-server', () => console.log("User entered server"));
 
 userRouter.get('/ledger', authenticateToken, userController.getLedgers);
 userRouter.post('/ledger', authenticateToken, userController.createLedger);
@@ -11,5 +10,7 @@ userRouter.get('/ledger/:ledgerId', authenticateToken, userController.getLedger)
 
 userRouter.get('/transactions/:ledgerId', authenticateToken, userController.getTransactions);
 userRouter.post('/transactions/:ledgerId', authenticateToken, userController.createTransactions);
+
+userRouter.get('/run-server', () => console.log("User entered server"));
 
 export default userRouter

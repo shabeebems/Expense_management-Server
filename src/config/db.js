9a44b5c89bcef69f0d6a9ee86cf26f78ff23224c
Muhammed Shabeeb <muhammedshabeeb330@gmail.com>
2025-08-31 17:16:27 +0530

    resolved merge conflicts

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ dotenv.config()
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
-        console.log('mongodb connect successful')
+        console.log('mongodb connect successfully')
     } catch (error) {
         console.error("MongoDB connection failed:", error);
     }

95c44d38250707ea45bfa7c4880e66746114fe86
Muhammed Shabeeb <muhammedshabeeb330@gmail.com>
2025-08-31 17:14:07 +0530

    feat(ui): add dummy message page

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ dotenv.config()
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
-        console.log('mongodb connect successfully')
+        console.log('mongodb connect successful')
     } catch (error) {
         console.error("MongoDB connection failed:", error);
     }
